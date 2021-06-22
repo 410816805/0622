74f190e173db84f066183dd7ca21ca309765d818
CrazyShape <a0917983200@gmail.com>
2021-06-22 18:26:14 +0800

    0622

--- app/src/main/java/tw/edu/pu/csim/tcyang/testdrag/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/testdrag/MainActivity.kt
@@ -1,14 +1,17 @@
 package tw.edu.pu.csim.tcyang.testdrag
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var img:ImageView
+    lateinit var a:ImageView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,5 +31,28 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
         })
+
+        a = findViewById(R.id.a)
+        a.setOnTouchListener(object:View.OnTouchListener{
+            override fun onTouch(p0: View?, motionEvent: MotionEvent?): Boolean {
+                if (motionEvent?.action == MotionEvent.ACTION_MOVE) {
+                    a.y = motionEvent.rawY-a.height
+                    a.x = motionEvent.rawX-a.width/2
+                }
+
+                return true
+            }
+        })
     }
+
+    fun CollisionTest(a: Rect, a1: Rect?): Boolean? {
+        if (a.intersect(a1!!)) {
+            a.visibility = View.GONE
+        }
+        else {
+
+        }
+        return true
+    }
+
 }
